b70bce710911b67f88dceb5a11ea1be3c3209857
apenkina <1303198728032013>
2019-09-03 15:03:32 +0600

    Broken

---
+++ build/resources/main/db/migration/V1__Employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+   id serial PRIMARY KEY,
+   first_name VARCHAR(100),
+   last_name VARCHAR(100),
+   amount INTEGER(100),
+   price DECIMAL(100),
+   category VARCHAR (100)
+)
--- src/main/resources/db/migration/V2__Employee.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE employee (
-   id serial PRIMARY KEY,
-   first_name VARCHAR(100),
-   last_name VARCHAR(100)
-)
